b11d4c290c8a4f6c536f59b940448792168a4bd0
Indra Purnomo <indpurno@gmail.com>
2022-08-15 15:33:02 +0700

    Final commit

--- src/main/java/BPB1/Repositories/GroupChat.java
+++ src/main/java/BPB1/Repositories/GroupChat.java
@@ -1,55 +1,220 @@
 package BPB1.Repositories;
 
+import BPB1.Basic.Basic;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.remote.LocalFileDetector;
+import org.openqa.selenium.support.PageFactory;
+
+import static BPB1.Basic.Basic.driver;
 
 public class GroupChat {
-    WebDriver driver;
-
-    public GroupChat(WebDriver driver) {this.driver = driver;}
-
-    @FindBy(xpath = "//*[@id=\"root\"]/div[3]/div[1]/div[1]/a/div")
-    WebElement GroupChatMenu;
-
-    @FindBy(xpath = "//*[@id=\"root\"]/div[3]/div[1]/div[2]/div/div[2]/div/div/div")
-    WebDriver TypeMessageHere;
-
-    @FindBy(xpath = "//*[@id=\"root\"]/div[3]/div[1]/div[2]/div/div[3]/div/div/svg")
-    WebDriver SendIcon;
-
-    @FindBy(xpath = "//*[@id=\"root\"]/div[3]/div[1]/div[2]/div/div[1]/div/svg")
-    WebDriver AttachFileIcon;
-
-    @FindBy(xpath = "//*[@id=\"list-groupChat-messages-section\"]/div/div/div[1]/div/div/div[1]/div[1]/svg")
-    WebDriver IcoonRoot;
-
-    @FindBy(xpath = "//*[@id=\"list-groupChat-messages-section\"]/div/div/div[1]/div/div/div[1]/div[2]/div[3]/div/div/div")
-    WebDriver DeleteMessege;
-
-    @FindBy(xpath = "/html/body/div[6]/div/div/div[3]/button")
-    WebDriver BtnDelete;
-
-    //Actions
-    public String clickGroupChatMenu() {
-        return  driver.getTitle();
+    public GroupChat() {
+        PageFactory.initElements(driver, "/companies/61eba2c85080f47b25ddc8f8");
     }
-    public String clickTypeMessageHere() {
-        return driver.getTitle();
+
+    /**
+     * Locator
+     */
+
+    By TypeMessageHere_input = By.xpath("//div[@class='fr-element fr-view']//p");
+    By Attachment_logo = By.xpath("//div[@class='CreateMessage_attachment__2Z8Rx']//*[name()='svg']");
+    By Send_logo = By.xpath("//*[@id=\"root\"]/div[3]/div[1]/div[2]/div/div[3]/div/div");
+    By indra_message = By.xpath("//*[@id=\"list-groupChat-messages-section\"]/div/div/div[2]/div/div/div[1]/div[2]/div/p");
+    By ThisIsTest_Message = By.xpath("(//p[contains(text(),'This is test')])[1]");
+    By DownArrow_logo = By.xpath("//*[@id=\"list-groupChat-messages-section\"]/div/div/div[3]/div/div/div[1]/div[1]");
+    By DeleteMessage_button = By.xpath("//*[@id=\"list-groupChat-messages-section\"]/div/div/div[3]/div/div/div[1]/div[2]/div[3]/div/div/div/p");
+    By GroupChat_feature = By.xpath("//h1[normalize-space()='Group Chat']");
+    By projectCard = By.xpath("//*[@id=\"root\"]/div[3]/div[4]/a/div");
+    By DownloadExcel = By.xpath("//*[@id=\"list-groupChat-messages-section\"]/div/div/div[1]/div/div/div[1]/div[2]/div[2]/a");
+    By testing_fordelete = By.xpath("//*[@id=\"list-groupChat-messages-section\"]/div/div/div[3]/div/div/div[1]/div[2]/div/p");
+    By DeleteButton_popup = By.xpath("/html/body/div[5]/div/div/div[3]/button/div");
+    By IndriMessage_select = By.xpath("//*[@id=\"root\"]/div[3]/div[2]/div[3]/div/div[2]/div/div/div/p");
+
+    //Title
+    By Bold_font = By.xpath("//*[@id=\"bold-2\"]");
+    By Italic_font = By.xpath("//*[@id=\"italic-2\"]");
+    By Underline_font = By.xpath("//*[@id=\"underline-2\"]");
+    By StrikeThrough_typographical = By.xpath("//*[@id=\"strikeThrough-2\"]");
+    By Orderedlist = By.xpath("//*[@id=\"formatOLSimple-2\"]");
+    By Emoticon = By.xpath("//button[@id='emoticons-2']//*[name()='svg']");
+    By Emoji_image = By.xpath("/html/body/div[5]/div[3]/span[1]");
+    By indri_messageFont = By.xpath("//*[@id=\"root\"]/div[3]/div[1]/div[2]/div/div[2]/div/div/div/p");
+
+    //Unorderedlist
+    By Unorderedlist = By.xpath("//*[@id=\"formatULOptions-2\"]");
+    // Down Arrow
+    By Unorderedlist_downarrow = By.id("formatULOptions-1");
+    //Default
+    By Default = By.xpath("//*[@id=\"dropdown-menu-formatULOptions-2\"]/div/div/ul/li[1]/a");
+    //Circle
+    By Circle = By.xpath("//*[@id=\"dropdown-menu-formatULOptions-2\"]/div/div/ul/li[2]/a");
+    //Disc
+    By Disc = By.xpath("//*[@id=\"dropdown-menu-formatULOptions-2\"]/div/div/ul/li[3]/a");
+    //Square
+    By Square = By.xpath("//*[@id=\"dropdown-menu-formatULOptions-2\"]/div/div/ul/li[4]/a");
+
+    //BackgroundColor
+    By BackgroundColor = By.id("backgroundColor-2");
+    //Color Red
+    By Color = By.xpath("//span[25]");
+    By OK_button = By.xpath("//button[normalize-space()='OK']");
+    By Delete_button = By.xpath("/html/body/div[5]/div[2]/span[28]");
+
+    //Assert
+    By MessageContent_testing = By.xpath("//*[@id=\"list-groupChat-messages-section\"]/div/div/div[1]/div/div/div[1]/div[2]/div/p");
+    By DeleteMessage_success = By.id("notistack-snackbar");
+    By BoldItalicEct_message = By.xpath("//*[@id=\"list-groupChat-messages-section\"]/div/div/div[1]/div/div/div[1]/div[2]/div/p/strong");
+    By Italic_message = By.xpath("//*[@id=\"list-groupChat-messages-section\"]/div/div/div[1]/div/div/div[1]/div[2]/div/p/em");
+    By Underline_message = By.xpath("//*[@id=\"list-groupChat-messages-section\"]/div/div/div[1]/div/div/div[1]/div[2]/div/p/u");
+    By StrikeThrough_message = By.xpath("//*[@id=\"list-groupChat-messages-section\"]/div/div/div[1]/div/div/div[1]/div[2]/div/p/s");
+    By Emoji_message = By.xpath("//*[@id=\"list-groupChat-messages-section\"]/div/div/div[1]/div/div/div[1]/div[2]/div/p/span");
+
+    /**
+     * Method
+     */
+
+    public void EnterTypeMessageHere(String user) {
+        driver.findElement(TypeMessageHere_input).sendKeys(user);
     }
-    public String SendIcon() {
-        return driver.getTitle();
+
+    public void ClickAttachment_logo() {
+        driver.findElement(Attachment_logo).click();
     }
-    public String AttachFileIcon() {
-        return driver.getTitle();
+
+    public void ClickSend_logo() { driver.findElement(Send_logo).click(); }
+
+    public void ClickThisIsTest_Message() {
+        driver.findElement(ThisIsTest_Message).click();
     }
-    public String IcoonRoot() {
-        return driver.getTitle();
+
+    public void clickindra_message() {
+        driver.findElement(indra_message).click();
     }
-    public String DeleteMessege() {
-        return driver.getTitle();
+
+    public void uploadMcWord() { driver.findElement(By.xpath("//div[@class='CreateMessage_attachment__2Z8Rx']")).sendKeys("C:\\Users\\user\\Desktop\\sekolahqa\\tugas\\Big_Project\\Doc_Test\\test_word");}
+
+    public void ClickDownArrow_logo() {
+        driver.findElement(DownArrow_logo).click();
     }
-    public String BtnDelete() {
-        return driver.getTitle();
+
+    public void ClickDeleteMessage() { driver.findElement(DeleteMessage_button).click(); }
+
+    public void clickGroupChat_feature() {
+        driver.findElement(GroupChat_feature).click();
+    }
+
+    public void clickProjectCard(){
+        driver.findElement(projectCard).click();
+    }
+
+    public void clickDownloadExcel() {
+        driver.findElement((DownloadExcel)).click();
+    }
+
+    public void clicktesting_fordelete() {
+        driver.findElement(testing_fordelete).click();
+    }
+
+    public void clickDeleteButton_popup() {
+        driver.findElement(DeleteButton_popup).click();
+    }
+
+    public void swifeleft_IndriMessage_select() { driver.findElement(By.xpath("//*[@id=\"root\"]/div[3]/div[1]/div[2]/div/div[2]/div/div/div/p")).sendKeys(Keys.CONTROL + "a");}
+
+    //Assert
+    public String AssertMessageContent_testing() { return driver.findElement(MessageContent_testing).getText(); }
+
+    public String PopupDeleteMessage_success() { return driver.findElement(DeleteMessage_success).getText(); }
+
+    public String AssertBoldItalicEct_message() { return driver.findElement(BoldItalicEct_message).getText(); }
+
+    public String AssertItalic_message() { return driver.findElement(Italic_message).getText(); }
+
+    public String AssertUnderline_message() { return driver.findElement(Underline_message).getText(); }
+
+    public String AssertStrikeThrough_message() { return driver.findElement(StrikeThrough_message).getText(); }
+
+    public String AssertEmoji_message() { return driver.findElement(Emoji_message).getText(); }
+
+
+    //Title
+
+    public void clickBold_font() {
+        driver.findElement(Bold_font).click();
+    }
+
+    public void clickItalic_font() {
+        driver.findElement(Italic_font).click();
+    }
+
+    public void clickUnderline_font() {
+        driver.findElement(Underline_font).click();
+    }
+
+    public void clickStrikeThrough_typographical() {
+        driver.findElement(StrikeThrough_typographical).click();
+    }
+
+    public void clickOrderedlist() {
+        driver.findElement(Orderedlist).click();
+    }
+
+    public void clickEmoticon() {
+        driver.findElement(Emoticon).click();
+    }
+
+    public void clickEmojiImage() {
+        driver.findElement(Emoji_image).click();
+    }
+
+    public void Indri_messageFont() {
+        driver.findElement(indri_messageFont).click();
+    }
+
+    ///Unorderedlist
+
+    public void clickUnorderedlist() {
+        driver.findElement(Unorderedlist).click();
+    }
+
+    public void clickUnorderedlist_downarrow() {
+        driver.findElement(Unorderedlist_downarrow).click();
+    }
+
+    public void clickDefault() {
+        driver.findElement(Default).click();
+    }
+
+    public void clickCircle() {
+        driver.findElement(Circle).click();
+    }
+
+    public void clickDisc() {
+        driver.findElement(Disc).click();
+    }
+
+    public void clickSquare() {
+        driver.findElement(Square).click();
+    }
+
+    //BackgroundColor
+
+    public void clickBackgroundColor() {
+        driver.findElement(BackgroundColor).click();
+    }
+
+    public void clickColor() {
+        driver.findElement(Color).click();
+    }
+
+    public void clickOK_button() {
+        driver.findElement(OK_button).click();
+    }
+
+    public void clickDelete_button() {
+        driver.findElement(Delete_button).click();
     }
 }
--- src/main/java/BPB1/Repositories/PrivateChat.java
+++ src/main/java/BPB1/Repositories/PrivateChat.java
@@ -1,4 +1,201 @@
 package BPB1.Repositories;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.PageFactory;
+
+import static BPB1.Basic.Basic.driver;
+
 public class PrivateChat {
+
+    public PrivateChat(){
+        PageFactory.initElements(driver, "/companies/61eba2c85080f47b25ddc8f8");
+    }
+
+    /**
+     * Locator
+     */
+
+    By Profile_member = By.xpath("//*[@id=\"root\"]/div[1]/div[2]/div[4]/div/span");
+    By PrivateChat_feature = By.xpath("//div[@aria-label='Private Chat']//div[@class='IconItem_container__2XsfF']//*[name()='svg']");
+    By SearchPeople_box = By.xpath("//input[@placeholder='Search people']");
+    By Message_logo = By.xpath("//*[name()='path' and contains(@d,'M4 4h16v12')]");
+    By TriWahyuni_profile = By.xpath("//p[normalize-space()='Tri Wahyuni_QA13']");
+    By LeftArrow_contact = By.xpath("//*[name()='path' and contains(@d,'M11.67 3.8')]");
+    By TypeMessageHere_input = By.xpath("//div[@class='fr-element fr-view']//p");
+    By Attachment_logo = By.xpath("//div[@class='CreateMessage_attachment__2Z8Rx']//*[name()='svg']");
+    By Send_logo = By.xpath("//*[@id=\"root\"]/div[3]/div[2]/div[3]/div/div[3]");
+    By dadada_message = By.xpath("//div[@class='Message_content__21YIN']//p[contains(text(),'dadadad')]");
+    By DownArrow_logo = By.xpath("//*[@id=\"list-messages-section\"]/div/div/div[2]/div/div/div[1]/div[1]");
+    By DeleteMessage_button = By.xpath("//*[@id=\"list-messages-section\"]/div/div/div[2]/div/div/div[1]/div[2]/div[3]/div/div/div/p");
+    By DeletePopUp_button = By.xpath("/html/body/div[4]/div/div/div[3]/button/div");
+    By testing_verify = By.xpath("//*[@id=\"list-messages-section\"]/div/div/div[2]/div/div/div[1]/div[2]/div/p");
+    By testing_fordelete = By.xpath("//*[@id=\"list-messages-section\"]/div/div/div[2]/div/div/div[1]/div[2]/div/p");
+
+    //Title
+    By Bold_font = By.id("bold-1");
+    By Italic_font = By.id("italic-1");
+    By Underline_font = By.id("underline-1");
+    By StrikeThrough_typographical = By.id("strikeThrough-1");
+    By Orderedlist = By.id("formatOLSimple-1");
+    By Emoticon = By.id("emoticons-1");
+    By Emoji_image = By.xpath("/html/body/div[4]/div[3]/span[1]");
+
+    //Unorderedlist
+    By Unorderedlist = By.id("formatUL-1");
+    // Down Arrow
+    By Unorderedlist_downarrow = By.id("formatULOptions-1");
+    //Default
+    By Default = By.xpath("//*[@id=\"dropdown-menu-formatULOptions-2\"]/div/div/ul/li[1]/a");
+    //Circle
+    By Circle = By.xpath("//*[@id=\"dropdown-menu-formatULOptions-1\"]/div/div/ul/li[2]/a");
+    //Disc
+    By Disc = By.xpath("//*[@id=\"dropdown-menu-formatULOptions-1\"]/div/div/ul/li[3]/a");
+    //Square
+    By Square = By.xpath("//*[@id=\"dropdown-menu-formatULOptions-1\"]/div/div/ul/li[4]/a");
+
+    //BackgroundColor
+    By BackgroundColor = By.id("backgroundColor-1");
+    //Color Red
+    By Color = By.xpath("//span[25]");
+    By OK_button = By.xpath("//button[normalize-space()='OK']");
+    By Delete_button = By.xpath("/html/body/div[4]/div[2]/span[28]");
+
+    //Assert
+    By StartChatting_private = By.xpath("//*[@id=\"root\"]/div[3]/div[2]/div[2]/p");
+    By PrivateChat_TriWahyuni = By.xpath("//*[@id=\"root\"]/div[2]/div/div[2]/div[1]/h1");
+    By RecentChat_private = By.xpath("//*[@id=\"root\"]/div[3]/div[1]/div[1]/div[1]/div[1]/h1");
+    By Contact_private = By.xpath("//*[@id=\"root\"]/div[3]/div[1]/div[1]/div[1]/div/h1");
+    By ThereInNoMember = By.xpath("//*[@id=\"root\"]/div[3]/div[1]/div[2]/div/span");
+    By ThereIsNoRecentChats = By.xpath("//*[@id=\"root\"]/div[3]/div[1]/div[2]/div/div/div/span");
+
+    /**
+     * Method
+     */
+
+    public void clickProfile_member() {
+        driver.findElement(Profile_member).click();
+    }
+
+    public void clickPrivateChat_feature() {
+        driver.findElement(PrivateChat_feature).click();
+    }
+
+    public void EnterSearchPeople_box(String user) {
+        driver.findElement(SearchPeople_box).sendKeys(user);
+    }
+
+    public void ClickMessage_logo() {
+        driver.findElement(Message_logo).click();
+    }
+
+    public void ClickTriWahyuni_profile() {
+        driver.findElement(TriWahyuni_profile).click();
+    }
+
+    public void ClickLeftArrow_contact() {
+        driver.findElement(LeftArrow_contact).click();
+    }
+
+    public void EnterTypeMessageHere_input(String user) {
+        driver.findElement(TypeMessageHere_input).sendKeys(user);
+    }
+
+    public void ClickAttachment_logo() {
+        driver.findElement(Attachment_logo).click();
+    }
+
+    public void ClickSend_logo() {
+        driver.findElement(Send_logo).click();
+    }
+
+    public void Clickdadada_message() {
+        driver.findElement(dadada_message).click();
+    }
+
+    public void ClickDownArrow_logo() {
+        driver.findElement(DownArrow_logo).click();
+    }
+
+    public void ClickDeleteMessage_button() {
+        driver.findElement(DeleteMessage_button).click();
+    }
+
+    public void clickDeletePopUp_button() { driver.findElement(DeletePopUp_button).click(); }
+
+    public void clicktesting_fordelete() {
+        driver.findElement(testing_fordelete).click();
+    }
+
+    public void swifelefttext_select() { driver.findElement(By.xpath("//*[@id=\"root\"]/div[3]/div[2]/div[3]/div/div[2]/div/div/div/p")).sendKeys(Keys.CONTROL + "a"); }
+
+    public String ValidateText_testing() {
+        driver.findElement(testing_verify).isDisplayed();
+        return driver.findElement(testing_verify).getText();
+    }
+
+    //Title
+
+    public void clickBold_font() {
+        driver.findElement(Bold_font).click();
+    }
+
+    public void clickItalic_font() {
+        driver.findElement(Italic_font).click();
+    }
+
+    public void clickUnderline_font() {
+        driver.findElement(Underline_font).click();
+    }
+
+    public void clickStrikeThrough_typographical() {
+        driver.findElement(StrikeThrough_typographical).click();
+    }
+
+    public void clickOrderedlist() {
+        driver.findElement(Orderedlist).click();
+    }
+
+    ///Unorderedlist
+
+    public void clickUnorderedlist() { driver.findElement(Unorderedlist).click();}
+
+    public void clickUnorderedDownArrow() { driver.findElement(Unorderedlist_downarrow).click(); }
+
+    public void clickCircle_Unordered() { driver.findElement(Circle).click(); }
+
+    public void clickDisc_Unordered() { driver.findElement(Disc).click(); }
+
+    public void clickSquare_Unordered() { driver.findElement(Square).click(); }
+
+    public void clickEmoticon() {
+        driver.findElement(Emoticon).click();
+    }
+
+    public void clickEmojiImage() {
+        driver.findElement(Emoji_image).click();
+    }
+
+    //Background Color
+
+    public void clickBackgroundColor() { driver.findElement(BackgroundColor).click(); }
+
+    public void clickColorRed() { driver.findElement(Color).click(); }
+
+    public void clickDelete_button() { driver.findElement(Delete_button).click(); }
+
+    //Assert
+
+    public String AssertStartChatting_private() { return driver.findElement(StartChatting_private).getText(); }
+
+    public String AssertPrivateChat_TriWahyuni() { return driver.findElement(PrivateChat_TriWahyuni).getText(); }
+
+    public String AssertRecentChat_private() { return driver.findElement(RecentChat_private).getText(); }
+
+    public String AssertContact_private() { return driver.findElement(Contact_private).getText(); }
+
+    public String AssertThereIsNoMember() { return driver.findElement(ThereInNoMember).getText(); }
+
+    public String AssertThersIsNoRecentChats() { return driver.findElement(ThereIsNoRecentChats).getText(); }
+
 }
